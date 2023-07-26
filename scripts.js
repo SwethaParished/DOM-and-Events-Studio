9d64ac56e17e1e815b323fb0bf44af635f640544
SwethaParished <swethaparished@gmail.com>
2023-07-25 22:47:55 -0500

    Done with the bonus mission

--- scripts.js
+++ scripts.js
@@ -1,56 +1,72 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 window.addEventListener("load", function(){
-    const takeOff = this.document.getElementById("takeoff");
-    const landing = this.document.getElementById("landing");
-    const missionAbort = this.document.getElementById("missionAbort");
-    const flightStatus = this.document.getElementById("flightStatus");
-    const bgcolor = this.document.getElementById("shuttleBackground");
-    const shuttleHeight = this.document.getElementById("spaceShuttleHeight");
-    let offsetDown = 0;
+    const takeOff = document.getElementById("takeoff");
+    const landing = document.getElementById("landing");
+    const missionAbort = document.getElementById("missionAbort");
+    const flightStatus = document.getElementById("flightStatus");
+    const bgcolor = document.getElementById("shuttleBackground");
+    const shuttleHeight = document.getElementById("spaceShuttleHeight");
+    const rocket = document.getElementById("rocket");
+    let rocketPosX = 0;
+    let rocketPosY = 0;
     takeOff.addEventListener("click", function(){
         let userInput = window.confirm("Confirm that the shuttle is ready for takeoff.");
         if(userInput){
             flightStatus.innerHTML = "Shuttle in flight."
             bgcolor.style.backgroundColor = 'blue';
-            shuttleHeight.innerHTML = 10000;
+            altitude = 10000;
+            shuttleHeight.innerHTML = altitude;
+            rocketPosY += 10;
+            rocket.style.marginBottom = rocketPosY + 'px';
         }
     })
     landing.addEventListener("click", function(){
-        let userInput = window.confirm("The shuttle is landing. Landing gear engaged.");
-        if(userInput){
+        alert("The shuttle is landing. Landing gear engaged.");
             flightStatus.innerHTML = "The shuttle has landed."
-            bgcolor.style.backgroundColor = 'green';
-            shuttleHeight.innerHTML = 0;
-        }
+            resetBack();
+           
     })
     missionAbort.addEventListener("click", function(){
         let userInput = window.confirm("Confirm that you want to abort the mission.");
         if(userInput){
             flightStatus.innerHTML = "Mission aborted."
-            bgcolor.style.backgroundColor = 'green';
-            shuttleHeight.innerHTML = 0;
+            resetBack();
         }
     })
-    document.getElementById("up").onclick = function() {  
-        document.getElementById("up").style.backgroundColor = 'grey';  
+    document.addEventListener("click", function(event){
+        let bkgWidth = parseInt(window.getComputedStyle(shuttleBackground).getPropertyValue('width'));
+        console.log("Width: ", bkgWidth);
+        if(event.target.id === "up" && altitude < 270000){
+            rocketPosY += 10;
+            rocket.style.marginBottom = rocketPosY + 'px';
+            altitude += 10000;
+            shuttleHeight.innerHTML = altitude;
+        }
+        if(event.target.id === "down" && altitude >0){
+            rocketPosY -= 10;
+            rocket.style.marginBottom = rocketPosY + 'px';
+            altitude -= 10000;
+            shuttleHeight.innerHTML = altitude;
+        }
+        if(event.target.id === "left" && rocketPosX > -(bkgWidth / 2) - 10){
+            rocketPosX -= 10;
+            rocket.style.marginLeft = rocketPosX + 'px';
+        }
+        if(event.target.id === "right" && rocketPosX < (bkgWidth / 2) - 10){
+            rocketPosX += 10;
+            rocket.style.marginLeft = rocketPosX + 'px';
+        }
+    })
 
+    function resetBack(){
+        bgcolor.style.backgroundColor = 'green';
+        altitude = 0;
+        shuttleHeight.innerHTML = altitude;
+        rocketPosX = 0;
+        rocketPosY = 0;
+        rocket.style.marginBottom = rocketPosY + 'px';
+        rocket.style.marginLeft = rocketPosX + 'px';
     }
-    up.addEventListener("click", function(){
-        document.getElementById("up").onclick = function() {  
-            
-        let up = parseInt(rocket.style.up);
-       rocket.style.position = "relative";
-       rocket.style.up = (upbtn+10)+"px";
-        }
-    })
-    down.addEventListener("click", function(){
-        
-        offsetDown += 10;
-        let down = parseInt(rocket.style.down);
-        rocket.style.position = "relative";
-        rocket.style.down = (offsetDown)+"px";
-        e.preventDefault();
-     })
    
 })
